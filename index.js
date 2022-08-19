40e29952d1c39d1f89d60db941971df4ad9f6a4f
Terminator <salmanvhora77777@gmail.com>
2022-08-19 22:12:43 +0530

    Changed Session ID

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ process.setMaxListeners(Infinity);
 const username = ''; // your temp instagram username for session cookie
 const password = ''; // your password for session coookie
 
-const cookiee = 'csrftoken=gCOg4ozcPAWp8LWP5wuxMQUxzVvByLwR; rur=VLL; ds_user_id=52969164045; sessionid=52969164045%3AU5XD8unSSCXBo9%3A1%3AAYcIrxtTAvRr7U-2uGqn6DpdZ_iabcXTq5pHiHVhTA';
+const cookiee = 'csrftoken=gCOg4ozcPAWp8LWP5wuxMQUxzVvByLwR; rur=VLL; ds_user_id=52969164045; sessionid=52969164045%3AJpMwfYAqXvagW5%3A16%3AAYepMy00N3_7TLHinu8mfb70Q0Jg5joRhiGojnZLxg';
 /* How To Get Cookie 
 Deploy Your App On Server The Visit Bellow Link
 
